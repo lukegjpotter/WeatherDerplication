1e60291d9996d575fcacf8cfb7f1229f03e17716
Luke GJ Potter <lukegjpotter@gmail.com>
2013-06-23 19:57:18 +0100

    Fixed compilation errors

--- WeatherDerplication/build/source/r/debug/com/github/lukegjpotter/app/derpyweather/R.java
+++ WeatherDerplication/build/source/r/debug/com/github/lukegjpotter/app/derpyweather/R.java
@@ -8,25 +8,134 @@
 package com.github.lukegjpotter.app.derpyweather;
 
 public final class R {
+    public static final class array {
+        public static final int default_city_names=0x7f050000;
+        public static final int default_city_zipcodes=0x7f050001;
+    }
     public static final class attr {
+    }
+    public static final class dimen {
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
+        public static final int widget_text_size=0x7f060002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int widget_bg_normal=0x7f020001;
     }
     public static final class id {
-        public static final int city_detail=0x7f060002;
-        public static final int city_detail_container=0x7f060000;
-        public static final int city_list=0x7f060001;
+        public static final int add_city_button=0x7f0a0006;
+        public static final int add_city_checkbox=0x7f0a0005;
+        public static final int add_city_edit_text=0x7f0a0004;
+        public static final int add_city_item=0x7f0a0022;
+        public static final int chance_of_precipitation=0x7f0a0012;
+        public static final int chance_of_precipitation_label=0x7f0a0011;
+        public static final int cities=0x7f0a0002;
+        public static final int city_detail=0x7f0a0014;
+        public static final int city_detail_container=0x7f0a0000;
+        public static final int city_list=0x7f0a0001;
+        public static final int containerLinearLayout=0x7f0a0008;
+        public static final int daily_forecast_bitmap=0x7f0a0015;
+        public static final int daily_forecast_description=0x7f0a0017;
+        public static final int day_of_week=0x7f0a0016;
+        public static final int feels_like=0x7f0a000e;
+        public static final int feels_likeTextView=0x7f0a001f;
+        public static final int feels_like_label=0x7f0a000d;
+        public static final int forecast_image=0x7f0a000a;
+        public static final int forecast_layout=0x7f0a0009;
+        public static final int forecast_replacer=0x7f0a0003;
+        public static final int forecast_table=0x7f0a0018;
+        public static final int high_temperature=0x7f0a001a;
+        public static final int high_temperature_label=0x7f0a0019;
+        public static final int humidity=0x7f0a0010;
+        public static final int humidityTextView=0x7f0a0020;
+        public static final int humidity_label=0x7f0a000f;
+        public static final int loading_message=0x7f0a0013;
+        public static final int location=0x7f0a0007;
+        public static final int low_temperature=0x7f0a001c;
+        public static final int low_temperature_label=0x7f0a001b;
+        public static final int precipitationTextView=0x7f0a0021;
+        public static final int temperature=0x7f0a000c;
+        public static final int temperatureTextView=0x7f0a001e;
+        public static final int temperature_label=0x7f0a000b;
+        public static final int weatherImageView=0x7f0a001d;
     }
     public static final class layout {
         public static final int activity_city_detail=0x7f030000;
         public static final int activity_city_list=0x7f030001;
         public static final int activity_city_twopane=0x7f030002;
-        public static final int fragment_city_detail=0x7f030003;
+        public static final int activity_weather_viewer=0x7f030003;
+        public static final int add_city_dialog=0x7f030004;
+        public static final int city_list_item=0x7f030005;
+        public static final int five_day_forecast_layout=0x7f030006;
+        public static final int forecast_fragment_layout=0x7f030007;
+        public static final int fragment_city_detail=0x7f030008;
+        public static final int single_forecast_layout_landscape=0x7f030009;
+        public static final int single_forecast_layout_portrait=0x7f03000a;
+        public static final int weather_app_widget_layout=0x7f03000b;
+    }
+    public static final class menu {
+        public static final int weather_viewer=0x7f090000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int title_city_detail=0x7f040001;
+        public static final int action_settings=0x7f070000;
+        public static final int add_city_button_text=0x7f070001;
+        public static final int add_city_checkbox_text=0x7f070002;
+        public static final int add_city_dialog_bundle_key=0x7f070003;
+        public static final int add_city_dialog_title=0x7f070004;
+        public static final int add_new_city=0x7f070005;
+        public static final int api_key=0x7f070006;
+        public static final int app_name=0x7f070007;
+        public static final int chance_of_precipitation=0x7f070008;
+        public static final int chance_of_precipitation_label_string=0x7f070009;
+        public static final int city=0x7f07000a;
+        public static final int city_dialog_cancel=0x7f07000b;
+        public static final int city_dialog_delete=0x7f07000c;
+        public static final int city_dialog_message_postfix=0x7f07000d;
+        public static final int city_dialog_message_prefix=0x7f07000e;
+        public static final int city_dialog_preferred=0x7f07000f;
+        public static final int click_widget_message=0x7f070010;
+        public static final int country=0x7f070011;
+        public static final int current_conditions=0x7f070012;
+        public static final int date_time=0x7f070013;
+        public static final int day_icon=0x7f070014;
+        public static final int day_of_week=0x7f070015;
+        public static final int day_prediction=0x7f070016;
+        public static final int default_zipcode=0x7f070017;
+        public static final int duplicate_zipcode_error=0x7f070018;
+        public static final int feels_like=0x7f070019;
+        public static final int feels_like_label_string=0x7f07001a;
+        public static final int five_day_forecast=0x7f07001b;
+        public static final int forecast_list=0x7f07001c;
+        public static final int high=0x7f07001d;
+        public static final int high_temperature_label_string=0x7f07001e;
+        public static final int hourly_forecast=0x7f07001f;
+        public static final int humidity=0x7f070020;
+        public static final int humidity_label_string=0x7f070021;
+        public static final int icon=0x7f070022;
+        public static final int invalid_zipcode_error=0x7f070023;
+        public static final int last_city_warning=0x7f070024;
+        public static final int loading_message=0x7f070025;
+        public static final int location=0x7f070026;
+        public static final int location_url_pre_zipcode=0x7f070027;
+        public static final int low=0x7f070028;
+        public static final int low_temperature_label_string=0x7f070029;
+        public static final int null_data_toast=0x7f07002a;
+        public static final int post_condition_url=0x7f07002b;
+        public static final int pre_condition_url=0x7f07002c;
+        public static final int pre_zipcode_url=0x7f07002d;
+        public static final int save=0x7f07002e;
+        public static final int state=0x7f07002f;
+        public static final int temperature=0x7f070030;
+        public static final int temperature_label_string=0x7f070031;
+        public static final int temperature_unit=0x7f070032;
+        public static final int title_activity_weather_viewer=0x7f070033;
+        public static final int title_city_detail=0x7f070034;
+        public static final int update_preferred_action=0x7f070035;
+        public static final int weather_options=0x7f070036;
+        public static final int zipcode=0x7f070037;
+        public static final int zipcode_extra=0x7f070038;
+        public static final int zipcode_prompt=0x7f070039;
     }
     public static final class style {
         /** 
@@ -36,9 +145,12 @@ public final class R {
         
  API 11 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int weather_widget_provider_info=0x7f040000;
     }
 }
