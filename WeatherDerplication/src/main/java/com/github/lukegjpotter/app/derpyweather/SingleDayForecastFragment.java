cecfaf4c21122ae27a57b7a69ab523a8eb0af0a7
Luke GJ Potter <lukegjpotter@gmail.com>
2013-07-23 22:20:55 +0100

    Implemented onCreateView method.

--- WeatherDerplication/src/main/java/com/github/lukegjpotter/app/derpyweather/SingleDayForecastFragment.java
+++ WeatherDerplication/src/main/java/com/github/lukegjpotter/app/derpyweather/SingleDayForecastFragment.java
@@ -12,7 +12,9 @@ package com.github.lukegjpotter.app.derpyweather;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -103,7 +105,7 @@ public class SingleDayForecastFragment extends ForecastFragment {
         outState.putString(FEELS_LIKE_KEY,    feelsLikeTextView.getText().toString());
         outState.putString(HUMIDITY_KEY,      humidityTextView.getText().toString());
         outState.putString(PRECIPITATION_KEY, chanceOfPrecipitationTextView.getText().toString());
-        outState.putParcelable(IMAGE_KEY,     conditionsBitmap);
+        outState.putParcelable(IMAGE_KEY, conditionsBitmap);
     }
 
     /**
@@ -125,5 +127,45 @@ public class SingleDayForecastFragment extends ForecastFragment {
     private void setZipcode(String zc) {
 
         this.zipCode = zc;
+    }
+
+    /**
+     * Inflates this Fragment's layout from XML.
+     *
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Use the given LayoutInflater to inflate layout stored in forecast_fragment_layout.xml.
+        View rootView = inflater.inflate(R.layout.forecast_fragment_layout, null);
+
+        // Get the TextView in the Fragment's layout hierarchy.
+        forecastView                  = rootView.findViewById(R.id.forecast_layout);
+        loadingTextView               = (TextView)  rootView.findViewById(R.id.loading_message);
+        locationTextView              = (TextView)  rootView.findViewById(R.id.location);
+        temperatureTextView           = (TextView)  rootView.findViewById(R.id.temperature);
+        feelsLikeTextView             = (TextView)  rootView.findViewById(R.id.feels_like);
+        humidityTextView              = (TextView)  rootView.findViewById(R.id.humidity);
+        chanceOfPrecipitationTextView = (TextView)  rootView.findViewById(R.id.chance_of_precipitation);
+        conditionImageView            = (ImageView) rootView.findViewById(R.id.forecast_image);
+
+        context = rootView.getContext();
+
+        return rootView;
+    }
+
+    /**
+     * Called when the parent Activity is created.
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
     }
 }
