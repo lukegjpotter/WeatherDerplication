553700c596a2221e44fad4db9f82ea8cb33b7bfb
Luke GJ Potter <lukegjpotter@gmail.com>
2013-07-25 21:21:36 +0100

    Added ReadLocationTask class

---
+++ WeatherDerplication/src/main/java/com/github/lukegjpotter/app/derpyweather/ReadLocationTask.java
@@ -0,0 +1,21 @@
+package com.github.lukegjpotter.app.derpyweather;
+
+import android.os.AsyncTask;
+
+/**
+ * ReadLocationTask.java
+ *
+ * @author Luke GJ Potter - lukegjpotter
+ * @date 25/Jul/13
+ * @version 1.0
+ *
+ * Description:
+ *     This class reads location information in a background thread.
+ */
+class ReadLocationTask extends AsyncTask<Object, Object, String> {
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        return null;
+    }
+}
